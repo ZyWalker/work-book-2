10be4ac254ae18a80b89a9969969f29155ec0cf4
Zyaire Walker <zyairewalker58@gmail.com>
2023-04-25 10:24:33 -0400

    1st changes

---
+++ program.js
@@ -0,0 +1,12 @@
+//var firstName;
+//firstName = "zyaire";
+//lastName = "walker";
+//console.log
+//console.log("hello world " + firstName + " " + lastName);
+
+var customerId = 555;
+var firstName = "Zyaire";
+var customerLastName = "Walker";
+console.log(customerId);
+console.log(customerFirstName);
+console.log(customerLastName);
