d5710e0a4722a4110c1cd890a6092d87a382a2c7
Zyaire Walker <zyairewalker58@gmail.com>
2023-04-28 09:50:14 -0400

    Website tinkering

---
+++ try-again.js
@@ -0,0 +1,15 @@
+let currentHour = 8;
+if (currentHour == 0 || currentHour <= 9 ) {
+    console.log("Good morning!");
+}
+else if (currentHour == 10 || currentHour <= 16 ) {
+    console.log("Good Day!");
+}
+else ( currentHour == 17 || currentHour <= 24 ) 
+    console.log("Good Evening!");
+
+
+console.log("")
+
+
+
